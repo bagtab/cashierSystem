a700787520a692fa0807ae54d5920e05780bbac1
bagtab <mrjohannisson@hotmail.com>
2019-05-26 15:39:23 +0200

    added comment

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -20,9 +20,6 @@ public class Controller {
 		register = new Register();
 		quantity = 1;
 	}
-	/**
-	 * initalizes a new sale
-	 */
 	public void startNewSale() {
 		// TODO Auto-generated method stub
 		sale = new Sale();
